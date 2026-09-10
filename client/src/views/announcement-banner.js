214d9e8246fe504aa8bf51d969282f55402fbec1
sprihozhiy <sprihozhiy@gmail.com>
2026-09-10 12:54:21 -0600

    update the copy

--- client/src/views/announcement-banner.js
+++ client/src/views/announcement-banner.js
@@ -6,8 +6,8 @@ export default () =>
   isLiquidMainnet
     ? <div className="announcement-banner">
         <p className="announcement-banner-message">
-          Liquid Network activity is currently paused due to a security incident. For more updates follow{' '}
-          <a href="https://x.com/Liquid_BTC" target="_blank" rel="noopener">@Liquid_BTC</a>.
+          Issued-asset transfers have resumed. LBTC transfers and peg-out operations remain paused. Updates:{' '}
+          <a href="https://x.com/Liquid_BTC" target="_blank" rel="noopener">@Liquid_BTC</a>
         </p>
         <button className="announcement-banner-close" aria-label="Dismiss announcement">
           <CloseIcon className="announcement-banner-close-icon" />
